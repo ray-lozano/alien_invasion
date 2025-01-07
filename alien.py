75dde7d1b6882dbb02b6769205eda6602ad9ebb9
Raymond Lozano <ray-lozano@live.com>
2025-01-07 15:10:20 -0800

    aliens now move back and forth on screen, drop down when reaching edge

--- alien.py
+++ alien.py
@@ -1,5 +1,4 @@
 import pygame
-
 from pygame.sprite import Sprite
 
 
@@ -10,6 +9,7 @@ class Alien(Sprite):
         """Initialize the alien and set its starting position."""
         super().__init__()
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
 
         # Load the alien image and set its rect attribute.
         self.image = pygame.image.load('images/alien.bmp')
@@ -21,3 +21,15 @@ class Alien(Sprite):
 
         # Store the alien's exact horizontal position.
         self.x = float(self.rect.x)
+
+    def check_edges(self):
+        """Check if alien is at the screen, return true if so."""
+        screen_rect = self.screen.get_rect()
+        return (self.rect.right >= screen_rect.right) or (self.rect.left <= 0)
+
+    def update(self):
+        """Move the alien to the right."""
+        self.x += self.settings.alien_speed * self.settings.fleet_direction
+        self.rect.x = self.x
+
+    
--- alien_invasion.py
+++ alien_invasion.py
@@ -39,6 +39,7 @@ class AlienInvasion:
             self._check_events()
             self.ship.update()
             self._update_bullets()
+            self._update_aliens()
             self._update_screen()            
             self.clock.tick(60)     # Have the game run at 60 frames per second.
 
@@ -46,7 +47,7 @@ class AlienInvasion:
         """Respond to keypresses and mouse events."""
         # Access events that Pygame detects with pygame.event.get()
         # pygame.event.get() returns a list of events that have 
-        # taken plkace since the last time this function was called.
+        # taken place since the last time this function was called.
         for event in pygame.event.get(): 
             # If the player closes the window, then exit.       
             if event.type == pygame.QUIT:
@@ -90,6 +91,11 @@ class AlienInvasion:
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
 
+    def _update_aliens(self):
+        """Update the positions of all aliens in the fleet."""
+        self._check_fleet_edges()
+        self.aliens.update()
+
     def _create_fleet(self):
         """Create the fleet of aliens."""
         # Create an alien and keep adding aliens until there's no room left.
@@ -114,6 +120,19 @@ class AlienInvasion:
         new_alien.rect.x = x_position
         new_alien.rect.y = y_position
         self.aliens.add(new_alien)
+
+    def _check_fleet_edges(self):
+        """Respond appropriately if any aliens have reached an edge."""
+        for alien in self.aliens.sprites():
+            if alien.check_edges():                 # Checking if any alien touches the edge of the screen.
+                self._change_fleet_direction()      # Changes the direction if a single alien touches the screen to either 1 or -1.
+                break
+    
+    def _change_fleet_direction(self):
+        """Drop the entire fleet and change the fleet's direction."""
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
     
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""

2037f45911f1133eff614cd071a5f9fb1deef323
Raymond Lozano <ray-lozano@live.com>
2025-01-10 09:53:05 -0800

    refills the screen with aliens if all are destroyed

--- alien_invasion.py
+++ alien_invasion.py
@@ -91,6 +91,11 @@ class AlienInvasion:
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
 
+        if not self.aliens:
+            # Destroy existing bullets and create new fleet.
+            self.bullets.empty()
+            self._create_fleet()
+
         # Check for any bullets that have hit aliens.
         # If so, get rid of the bullet and the alien.
         collisions = pygame.sprite.groupcollide(self.bullets, self.aliens, True, True) # Change the first boolean to false for creating a laser beam.

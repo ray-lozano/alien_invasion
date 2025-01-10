a56be2791f1ca965383dec1b444532220c765831
Raymond Lozano <ray-lozano@live.com>
2025-01-10 09:51:11 -0800

    added collision between bullets and ships

--- alien_invasion.py
+++ alien_invasion.py
@@ -91,6 +91,10 @@ class AlienInvasion:
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
 
+        # Check for any bullets that have hit aliens.
+        # If so, get rid of the bullet and the alien.
+        collisions = pygame.sprite.groupcollide(self.bullets, self.aliens, True, True) # Change the first boolean to false for creating a laser beam.
+
     def _update_aliens(self):
         """Update the positions of all aliens in the fleet."""
         self._check_fleet_edges()
